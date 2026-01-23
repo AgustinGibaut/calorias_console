f13fedb18cd706cec90ffef2f795f00d8e538e2f
Agustin G <agusgibaut693@gmail.com>
2026-01-23 02:51:52 -0300

    mantenimiento

--- db.py
+++ db.py
@@ -3,14 +3,9 @@ from mysql.connector import Error
 from dotenv import load_dotenv
 import os
 
-# Cargar las variables del .env
 load_dotenv()
 
 def create_connection():
-    """
-    Crea y devuelve la conexión a la base de datos usando variables del .env.
-    Retorna None si ocurre un error.
-    """
     try:
         connection = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
@@ -19,8 +14,8 @@ def create_connection():
             database=os.getenv("DB_NAME")
         )
         if connection.is_connected():
-            print("✅ Conexión a MySQL exitosa")
+            print(" Conexión a MySQL exitosa")
             return connection
     except Error as e:
-        print(f"❌ Error conectando a MySQL: {e}")
+        print(f" Error conectando a MySQL: {e}")
         return None
